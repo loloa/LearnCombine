5b549a3bf4ce4ecbaf7da41c7e3c0ea6dc1b8545
Lilia Lashin <lilia@tap.pm>
2021-11-24 14:24:26 +0200

    timer

--- LearnCombine/LearnCombine/ViewController.swift
+++ LearnCombine/LearnCombine/ViewController.swift
@@ -6,12 +6,51 @@
 //
 
 import UIKit
+import Combine
+
+extension Notification.Name {
+    static let example = Notification.Name("example")
+}
 
 class ViewController: UIViewController {
 
+    
+    var cancellable: AnyCancellable?
+    var tcancellable: AnyCancellable?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.cancellable = NotificationCenter.Publisher(center: .default, name: .example, object: nil)
+            .sink { notification in
+                print(notification)
+            }
+        
+        //post notification
+//        NotificationCenter.default.post(name: .example, object: nil)
+        
+        // start automatically
+        
+         
+        var secondsStart = 0
+        
+        self.tcancellable = Timer.publish(every: 1, on: .main, in: .default)
+            .autoconnect()
+            .sink { [unowned self] date in
+ 
+                secondsStart += 1
+                
+                if secondsStart <= 2 {
+                    print(secondsStart)
+                    NotificationCenter.default.post(name: .example, object: nil)
+                } else {
+                    self.tcancellable?.cancel()
+                    self.cancellable?.cancel()
+                }
+                
+               
+            }
+        
     }
 
 
